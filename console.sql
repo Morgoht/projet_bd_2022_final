147659a6f2610dea75f1df31fb7f8a90852aad7a
Amaury Andrade <amaury.andrade@student.vinci.be>
2022-11-29 15:54:29 +0100

    app centrale java fini

--- console.sql
+++ console.sql
@@ -44,6 +44,7 @@ CREATE TABLE projet.inscriptions_cours(
     id_etudiant INTEGER NOT NULL REFERENCES projet.etudiants(id_etudiant)
 );
 ---------------------   INSERTS   ---------------------
+/*
 ------ insert etudiant -----
 INSERT INTO projet.etudiants VALUES (DEFAULT, 'Andrade', 'Amaury', 'amaury.andrade@student.vinci.be', NULL  );
 INSERT INTO projet.etudiants VALUES (DEFAULT, 'Touat', 'Alexandre', 'alexandre.touat@student.vinci.be', NULL  );
@@ -76,7 +77,7 @@ INSERT INTO projet.inscriptions_groupe VALUES (2,3);
 INSERT INTO projet.inscriptions_groupe VALUES (2,4);
 INSERT INTO projet.inscriptions_groupe VALUES (3,5);
 
-
+*/
 ----------------------------------------- APP CENTRALE ----------------------------------------------
 CREATE OR REPLACE FUNCTION projet.ajouter_cours (new_nom varchar(20),new_code_cours varchar(20), new_bloc char, new_credit INTEGER) RETURNS INTEGER AS $$
     DECLARE
@@ -181,24 +182,28 @@ WHERE p.id_projet IS NOT NULL
 group by p.id_cours, p.id_projet, p.nom, p.id_cours, i.id_etudiant, g.etat;
 
 CREATE OR REPLACE  VIEW projet.vue_groupe_projet AS
-    SELECT  g.num_groupe as Numéro, e.nom, e.prenom, g.etat AS "validé ?",
+    SELECT  p.id_projet,  g.num_groupe as Numéro, e.nom, e.prenom,
     CASE WHEN g.etat = 'définitif' THEN 'Vrai' ELSE 'False' END "Validé",
     CASE WHEN count(i.id_etudiant) = g.nbr_membre THEN 'True' ELSE 'False' END "Complet"
     FROM projet.projets p, projet.groupes g, projet.etudiants e, projet.inscriptions_groupe i, projet.inscriptions_groupe i2
     WHERE p.id_projet = g.id_projet AND g.id_groupe = i.id_groupe
         AND i.id_etudiant = e.id_etudiant AND i.id_groupe = i2.id_groupe
-    GROUP BY g.num_groupe, e.nom, e.prenom, g.etat, g.nbr_membre;
+    GROUP BY p.id_projet, g.num_groupe, e.nom, e.prenom, g.etat, g.nbr_membre
+    ORDER BY g.num_groupe;
 
 
 
 
+SELECT * FROM projet.etudiants;
+SELECT * FROM projet.cours;
 
 /*
+SELECT DISTINCT * FROM projet.vue_projets;
 SELECT * FROM projet.vue_groupe_projet;
+SELECT * FROM projet.vue_cours;
+SELECT  numéro, nom, prenom, "Validé", "Complet" FROM projet.vue_groupe_projet WHERE id_projet = 1;
 SELECT * FROM projet.groupes WHERE id_groupe = 1;
 SELECT * FROM projet.groupes g WHERE g.etat = 'définitif';
-SELECT DISTINCT * FROM projet.vue_projets;
-SELECT * FROM projet.vue_cours;
 SELECT * FROM projet.projets;
 SELECT DISTINCT g.id_groupe, count(i.id_etudiant) as "inscris" FROM projet.groupes g, projet.inscriptions_groupe i
 WHERE i.id_groupe = g.id_groupe AND g.nbr_membre != (SELECT count(i.id_etudiant) FROM projet.inscriptions_groupe i WHERE i.id_groupe = g.id_groupe )
@@ -211,15 +216,11 @@ select * from projet.inscriptions_groupe;
 
 
 
-
-SELECT * FROM projet.groupes;
-
-SELECT projet.valider_tous_les_groupes(2);
-SELECT projet.valider_groupe(2,2);
+SELECT projet.ajouter_cours('test','BINV1112','1',1);
 --------------------- Appel de procédure --------------------
 /*
+
 SELECT projet.ajouter_groupe_projet(2,1,3);
-SELECT projet.ajouter_cours('test','BINV1112','1',1);
 SELECT projet.ajouter_cours('NeuroPsy','BINV2103','2',4);
 SELECT projet.ajouter_etudiant('Test','Arnaud', 'test.arnaud@student.vinci.be');
 SELECT projet.inscription_cours(1,1);
